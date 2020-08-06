75fd522d810b619ccbbd7b028cb4aebad797465c
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-06 17:21:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -23,10 +23,8 @@ def key_for_min_value(name_hash)
     if min_value > value
       min_value = value
       name_value = key
-    elsif
-     return nil
     end
-    
+    return nil
   end
   name_value
 end
