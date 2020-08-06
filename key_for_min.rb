adfb48d942777e7b41e78fed93d2602c6a3854a5
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-06 17:22:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,11 +17,11 @@
 # end
 
 def key_for_min_value(name_hash)
-  min_value = 700
+  minimum_value = 700
   name_value = " "
   name_hash.each do |key, value|
-    if min_value > value
-      min_value = value
+    if minimum_value > value
+      minimum_value = value
       name_value = key
     end
   end
