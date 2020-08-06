f9875079c4a515bcfb4df078fb85b3705d1d05f0
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-06 17:32:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,28 +3,24 @@
 
 def key_for_min_value(name_hash)
   minimum_value = 550
-  lowest_num = nil
-  name_hash.each do |name, number|
-    if lowest_num == nil 
-      answer = name
-      lowest_num = number
-    elsif lowest_num > number
-      answer = name
-      lowest_num = number
-    end
-  end
-  
-  answer
-end
-
-def key_for_min_value(name_hash)
-  minimum_value = 700
   name_value = nil
-  name_hash.each do |key, value|
-    if minimum_value > value
-      minimum_value = value
-      name_value = key
+  name_hash.each do |name, number|
+    if minimum_value > number 
+       minimum_value = number
+       name_value = name
     end
   end
   name_value
 end
+
+# def key_for_min_value(name_hash)
+#   minimum_value = 700
+#   name_value = nil
+#   name_hash.each do |key, value|
+#     if minimum_value > value
+#       minimum_value = value
+#       name_value = key
+#     end
+#   end
+#   name_value
+# end
